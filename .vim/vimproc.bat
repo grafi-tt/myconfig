3babf96a6a63f0f67569365ec4a425e9e96ae17c
Shunsuke Shimizu <grafi@grafi.jp>
2015-12-25 03:43:58 +0900

    add vimproc win

---
+++ .vim/vimproc.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+nmake -f make_msvc.mak nodebug=1 "SDK_INCLUDE_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include"
