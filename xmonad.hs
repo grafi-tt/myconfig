b82892f649043107664d792a969223b748d5c961
Shunsuke Shimizu <grafi@grafi.jp>
2016-08-14 20:03:34 +0900

    update xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,24 +1,28 @@
 import XMonad
 import XMonad.Config.Kde
 import qualified XMonad.StackSet as W -- to shift and float windows
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
 
 isNotification :: Query Bool
 isNotification = isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_NOTIFICATION"
 
 main = xmonad kde4Config
   { modMask = mod4Mask -- use the Windows button as mod
-  , manageHook = manageHook kdeConfig <+> myManageHook
+  , manageHook = manageHook kde4Config <+> myManageHook
+  , layoutHook = myLayoutHook
   }
 
+myLayoutHook = smartBorders $ layoutHook kde4Config
+
 myManageHook = composeAll . concat $
-  [ [ className   =? c --> doFloat           | c <- myFloats]
-  , [ title       =? t --> doFloat           | t <- myOtherFloats]
-  , [ className   =? c --> doF (W.shift "2") | c <- webApps]
-  , [ className   =? c --> doF (W.shift "3") | c <- ircApps]
+  [ [ className =? c --> doFloat                       | c <- myFloats ]
+  , [ className =? c --> doF (W.shift "2")             | c <- webApps ]
+  , [ className =? c --> doF (W.shift "3")             | c <- ircApps ]
   , [ isNotification --> doIgnore ]
   ]
-  where myFloats      = ["MPlayer", "Gimp"]
-        myOtherFloats = ["alsamixer"]
+  where myFloats      = ["Gimp", "MPlayer", "VirtualBox"]
         webApps       = ["Firefox"]
         ircApps       = ["Ksirc"]
