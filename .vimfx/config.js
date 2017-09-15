8e8139e373a7ec2639ab5d448412fc4b6cdfc660
Shunsuke Shimizu <grafi@grafi.jp>
2017-09-15 12:22:59 +0900

    initial vimfx config

---
+++ .vimfx/config.js
@@ -0,0 +1,14 @@
+// control
+vimfx.set('mode.normal.reload_config_file',          '.');
+
+// tab switching
+vimfx.set('mode.normal.tab_select_previous',        'b');
+vimfx.set('mode.normal.tab_select_next',            'f');
+
+// hit-a-hint
+vimfx.set('mode.normal.follow',                     'e');
+vimfx.set('mode.normal.follow_in_tab',              'E');
+vimfx.set('mode.normal.follow_in_focused_tab',      ';t');
+vimfx.set('mode.normal.follow_in_window',           ';w');
+vimfx.set('mode.normal.follow_in_private_window',   ';p');
+vimfx.set('mode.normal.follow_copy',                ';y');
