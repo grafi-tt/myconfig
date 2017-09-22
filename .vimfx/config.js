4d73944af0404f95e1d2ff6b3df029a468a6d004
Shunsuke Shimizu <grafi@grafi.jp>
2017-09-22 14:33:37 +0900

    hatebu

--- .vimfx/config.js
+++ .vimfx/config.js
@@ -1,5 +1,5 @@
 // control
-vimfx.set('mode.normal.reload_config_file',          '.');
+vimfx.set('mode.normal.reload_config_file',         '.');
 
 // tab and window
 vimfx.set('mode.normal.tab_select_previous',        'b');
@@ -19,3 +19,19 @@ vimfx.set('mode.normal.follow_in_focused_tab',      ';t');
 vimfx.set('mode.normal.follow_in_window',           ';w');
 vimfx.set('mode.normal.follow_in_private_window',   ';p');
 vimfx.set('mode.normal.follow_copy',                ';y');
+
+
+// openurl
+let register_openurl = function (name, url) {
+	vimfx.addCommand({
+		name: `openurl_${name}`,
+		description: `Open url ${url}`,
+		category: 'location',
+		order: 500,
+	}, ({vim}) => {
+		vim.window.gBrowser.loadOneTab(url, { inBackground: false, relatedToCurrent: true });
+	});
+};
+register_openurl('hatebu', 'http://b.hatena.ne.jp/grafi/bookmark');
+
+vimfx.set('custom.mode.normal.openurl_hatebu',      'gh');
