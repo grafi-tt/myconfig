0f43ea32861e99ef8f7da2a4bd06ae8799e1d29a
Shunsuke Shimizu <grafi@grafi.jp>
2017-09-22 14:47:31 +0900

    no title bar

--- rc.lua
+++ rc.lua
@@ -42,6 +42,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.wallpaper = awful.util.getdir("config") .. "wallpaper.png"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "konsole"
@@ -484,7 +485,7 @@ awful.rules.rules = {
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+      }, properties = { titlebars_enabled = false }
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
