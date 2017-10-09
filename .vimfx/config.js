63548e767d7f107bff86178b00b7f73c6acc9526
Shunsuke Shimizu <grafi@grafi.jp>
2017-10-09 19:25:10 +0900

    copy

--- .vimfx/config.js
+++ .vimfx/config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // control
 vimfx.set('mode.normal.reload_config_file',         '.');
 
@@ -22,7 +24,7 @@ vimfx.set('mode.normal.follow_copy',                ';y');
 
 
 // openurl
-let register_openurl = function (name, url) {
+let register_openurl = (name, url) => {
 	vimfx.addCommand({
 		name: `openurl_${name}`,
 		description: `Open url ${url}`,
@@ -35,3 +37,31 @@ let register_openurl = function (name, url) {
 register_openurl('hatebu', 'http://b.hatena.ne.jp/grafi/bookmark');
 
 vimfx.set('custom.mode.normal.openurl_hatebu',      'gh');
+
+
+// copy
+const gClipboardHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+	.getService(Components.interfaces.nsIClipboardHelper)
+
+let register_copy = (name, template) => {
+	vimfx.addCommand({
+		name: `copy_${name}`,
+		description: `Copy page url or title following the template "${template}"`,
+		category: 'location',
+		order: 500,
+	}, ({vim}) => {
+		let uri = vim.window.gBrowser.selectedBrowser.currentURI.spec;
+		let context = {
+			uri_ascii: uri.asciiSpec,
+			uri_unicode: uri.spec,
+			title: vim.window.gBrowser.selectedBrowser.contentTitle,
+		};
+		let s = template.replace(/\{\w+\}/g, (match) => {
+			return context[match.slice(1, -1)];
+		});
+		gClipboardHelper.copyString(s);
+	});
+};
+register_copy('title', '{title}');
+
+vimfx.set('custom.mode.normal.copy_title',          'yt');
